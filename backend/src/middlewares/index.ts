09c4bbc3218ef62d949a21a3f38848a4bae08b63
satendra03 <satendrakumarparteti.work@gmail.com>
2026-03-01 16:12:52 +0530

    setup: initialize backend structure with TypeScript, Express, and MongoDB

---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,21 @@
+// Middlewares
+
+import cors from "cors";
+import express, { type Application } from "express";
+// import { errorMiddleware } from "./error.middleware.js";
+// import { notFoundMiddleware } from "./notFound.middleware.js";
+// import { requestLogger } from "./logger.middleware.js";
+
+//  CORS, JSON, URL encoded
+export const registerMiddlewares = (app: Application) => {
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    // app.use(requestLogger);
+};
+
+// Not found and error handlers
+export const registerErrorHandlers = (app: Application) => {
+    // app.use(notFoundMiddleware);
+    // app.use(errorMiddleware);
+};
